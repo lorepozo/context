182a0a291a27f9351bd12382c4018de86cf7e54b
Lucas Morales <lucas@lucasem.com>
2017-05-08 10:52:17 -0400

    gplv3

--- src/ec.rs
+++ src/ec.rs
@@ -1,3 +1,20 @@
+// This file is part of Context, a program and library for machine learning.
+// This program is available at <https://docs.lucasem.com/context_src>
+// Copyright (C) 2017  Lucas E. Morales <lucas@lucasem.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 extern crate rand;
 extern crate serde_json;
 extern crate tempdir;
